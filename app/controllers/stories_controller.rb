99cf31e394f370b1d38e84eca76a72d8a68b76ae
Sui <suihsilan@gmail.com>
2024-01-29 20:36:24 +1100

    add stories create edit index update functions

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,44 @@
+class StoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_story, only: [:edit, :update, :destroy]
+  
+  def index
+    @stories = current_user.stories.order(created_at: :desc)
+  end
+
+  def new
+    @story = current_user.stories.new
+  end
+
+  def create
+    @story = current_user.stories.new(story_params)
+
+    if @story.save
+      redirect_to stories_path, notice: '新增成功！'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @story = Story.find(params[:id])
+  end
+
+  def update
+    @story = Story.find(params[:id])
+    if @story.update(story_params)
+      redirect_to stories_path, notice: '故事更新成功！'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def find_story
+    @stoty = current_user.stories.find(params[:id])
+  end
+  
+  def story_params
+    params.require(:story).permit(:title, :content)
+  end
+end
